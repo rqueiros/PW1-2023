f38bdde95797151ec7f554b8e539be99cdc5c131
Ricardo Queirós <ricardo.queiros@gmail.com>
2023-10-25 15:36:57 +0100

    project2

---
+++ vueupdown/src/components/Ex1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="value--" :disabled="value == minValue">-</button>
+    <span>{{ value }}</span>
+    <button @click="value++" :disabled="value == maxValue">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      minValue: 0,
+      maxValue: 10,
+      value: 0,
+    };
+  },
+  created () {
+    console.log("Instância Vue criada!");;
+  },
+  mounted () {
+    console.log("Instância Vue montada!");;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
