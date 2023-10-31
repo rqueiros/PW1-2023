494a37eeb4245e21dd0f272a6f1de3dbbcff1803
rqueiros <ricardo.queiros@gmail.com>
2023-10-31 23:22:04 +0000

    31-10

---
+++ vuePropsEvents/src/components/Catalog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }} -  ${{ product.price }}
+        <button @click="selectProduct(product)">Selecionar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    products: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    selectProduct(product) {
+      this.$emit("product-selected", product);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ vuePropsEvents/src/components/ProductsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Lista de Produtos</h2>
+    <Catalog
+      :products="productList"
+      @product-selected="handlerProductSelected"
+    />
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+import Catalog from "./Catalog.vue";
+export default {
+  components: {
+    Catalog,
+  },
+  data() {
+    return {
+      productList: [],
+      message: "",
+    };
+  },
+  created() {
+    this.productList = [
+      { id: 1, name: "p1", price: 100 },
+      { id: 2, name: "p2", price: 250 },
+      { id: 3, name: "p3", price: 72 },
+    ];
+  },
+  methods: {
+    handlerProductSelected(product) {
+      // Implemente a lógica necessária para lidar com o produto selecionado
+      // Por exemplo, atualização do estado, exibição de informações, etc.
+      this.message = `Produto selecionado: ${product.name}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
