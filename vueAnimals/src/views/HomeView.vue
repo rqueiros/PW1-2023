a9cc61ac3ca5e283e21bfedfc6cfac700793f29a
rqueiros <ricardo.queiros@gmail.com>
2023-11-09 11:51:26 +0000

    login component

--- vueAnimals/src/views/HomeView.vue
+++ vueAnimals/src/views/HomeView.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-  <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <img alt="Vue logo" class="logo" src="@/assets/animals.png" />
 </template>
--- vueAnimals/src/views/LoginView.vue
+++ vueAnimals/src/views/LoginView.vue
@@ -1,11 +1,38 @@
 <template>
-    <div>
+    <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent="login">
+      <v-text-field
+        v-model="username"
+        label="Username"
+      ></v-text-field>
 
-    </div>
+      <v-text-field
+        v-model="pass"
+        label="Password"
+      ></v-text-field>
+
+      <v-btn type="submit" block class="mt-2">Login</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                username: "",
+                pass:""
+            }
+        },
+        methods: {
+            login() {
+                if (JSON.parse(localStorage.users).some(user=>user.username==this.username && user.password == this.pass)) {
+                    this.$router.push({name:'animals'})
+                } else {
+                    alert('Invalid User!')
+                }                
+            }
+        },
         
     }
 </script>
