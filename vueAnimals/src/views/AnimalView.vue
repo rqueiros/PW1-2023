0d60e2d198d7de5e10e4cd9cb87712728da7febf
rqueiros <ricardo.queiros@gmail.com>
2023-11-09 11:33:06 +0000

    detail component

--- vueAnimals/src/views/AnimalView.vue
+++ vueAnimals/src/views/AnimalView.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    Animal {{ $route.params.id }}
-    <br>
-    <v-btn @click="$router.go(-1)">
-        BACK
-    </v-btn>
-    </div>
+    <h1>{{ animal.name }}</h1>   
+    <p><img :src="animal.image" alt=""></p>    
+    <p>{{ animal.desc }}</p>   
+    <p><v-btn @click="$router.go(-1)"> BACK </v-btn></p>
+    
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      animal: 0,
+    };
+  },
+  created() {
+    this.animal = JSON.parse(localStorage.animals).find(
+      (animal) => animal.id == this.$route.params.id
+    );
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
--- vueAnimals/src/views/PageNotFoundView.vue
+++ vueAnimals/src/views/PageNotFoundView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-
+        NO RESOURCE FOUND!
     </div>
 </template>
 
